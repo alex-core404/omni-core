20b748437d4646e3745f940cb37bbdfb6976132f
alex-core404 <asotnikov705@gmail.com>
2026-03-08 23:27:01 +0700

    feat: user auth - register and login with JWT

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
+from app.routers import auth
+
 app = FastAPI()
+app.include_router(auth.router)
 @app.get("/")
 async def root():
     return {"message": "Omni is alive!"}
